1c14964e83531726700ce37f6443d9d9e9f7bc61
davidkuo3 <davidhand32@gmail.com>
2026-02-25 11:04:23 +0800

    Initial commit: Personal webpage

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import os
+
+# Set page config
+st.set_page_config(
+    page_title="郭子熠 | 个人主页",
+    page_icon="✨",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+# Hide Streamlit elements with CSS
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .stApp {
+                margin: 0;
+                padding: 0;
+            }
+            iframe {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                border: none;
+            }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Read index.html
+with open("index.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Render HTML in full-screen iframe
+components.html(html_content, height=1000) # Height will be overridden by CSS iframe style above
